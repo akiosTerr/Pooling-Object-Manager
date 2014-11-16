bc77c11871235c4093e862c3499ae6a57a9915f7
luiz fernando <akios_the_terrible@live.com>
2014-11-16 03:35:32 -0200

    code update and extra scripts
    in this commit i put prints on the main script to do tests (wich you can
    delete if not needed), and add some extra scripts to show how i use the
    algorithm in my project,
    you can use asset_manager algorithm with no especific need of the other
    scripts.

--- asset_manager.cs
+++ asset_manager.cs
@@ -9,12 +9,12 @@ public class asset_manager : MonoBehaviour {
 
 	//prefabs
 	public GameObject fireWorks_GO;
-	public GameObject move_light1_GO;
-	public GameObject move_light2_GO;
+	public GameObject projetil1_GO;
+	public GameObject projetil2_GO;
 
 	//class instances
-	public poole_obj moveLight_1;
-	public poole_obj moveLight_2;
+	public poole_obj projetil1;
+	public poole_obj projetil2;
 	public poole_obj fireworks;
 
 	//can the list grow the amount of prefabs if needed? (you can also do code to limit how much can grow)
@@ -30,13 +30,13 @@ public class asset_manager : MonoBehaviour {
 	{
 		public List<GameObject> obj_list;
 
-		public poole_obj (GameObject GO,int start_amount) 
+		public poole_obj (GameObject g_obj,int start_amount) 
 		{
 			obj_list = new List<GameObject> ();
 
 			for (int i = 0; i < start_amount; i++) 
 			{
-				GameObject temp_obj = (GameObject)Instantiate(GO);
+				var temp_obj = (GameObject)Instantiate(g_obj);
 				temp_obj.SetActive(false);
 				obj_list.Add(temp_obj);
 			}
@@ -48,48 +48,55 @@ public class asset_manager : MonoBehaviour {
 	//as second argument put how many objects you will instantiate at first to be always ready to use(it can grow if needed if you enable "Can_Expand")
 	void Start () 
 	{
-		moveLight_1 = new poole_obj (move_light1_GO, 5);
-		moveLight_2 = new poole_obj (move_light2_GO, 5);
-		fireworks = new poole_obj (fireWorks_GO,5);
+		projetil1 = new poole_obj (projetil1_GO,10);
+		projetil2 = new poole_obj (projetil2_GO,10);
+		fireworks = new poole_obj (fireWorks_GO,6);
 	}
 
 
 	//methods to accsess the object from outside the script, each method returns a diferent object.
 	//to put a new object you have to mount a instance of "poole_obj" class on start, then make the same method pattern below whith the instance
-	public GameObject Get_moveLight1 ()
+	public GameObject Get_projetil1 ()
 	{
-		for (int i = 0; i < moveLight_1.obj_list.Count; i++) 
+		for (int i = 0; i < projetil1.obj_list.Count; i++) 
 		{
-			if(!moveLight_1.obj_list[i].activeInHierarchy)
+			if(!projetil1.obj_list[i].activeInHierarchy)
 			{
-				return moveLight_1.obj_list[i];
+				print("succes projetil 1");
+				return projetil1.obj_list[i];
 			}	
 		}
 		if (Can_Expand) 
 		{
-			GameObject extra_obj = (GameObject)Instantiate(move_light1_GO);
-			moveLight_1.obj_list.Add(extra_obj);
+			GameObject extra_obj = (GameObject)Instantiate(projetil1_GO);
+			projetil1.obj_list.Add(extra_obj);
+			print("projetil 1 expanded");
 			return extra_obj;
 		}
 
+		print ("projetil 1 send null");
+
 		return null;
 	}
-	public GameObject Get_moveLight2 ()
+	public GameObject Get_projetil2 ()
 	{
-		for (int i = 0; i < moveLight_2.obj_list.Count; i++) 
+		for (int i = 0; i < projetil2.obj_list.Count; i++) 
 		{
-			if(!moveLight_2.obj_list[i].activeInHierarchy)
+			if(!projetil2.obj_list[i].activeInHierarchy)
 			{
-				return moveLight_2.obj_list[i];
+				print("succes projetil 2");
+				return projetil2.obj_list[i];
 			}	
 		}
 		if (Can_Expand) 
 		{
-			GameObject extra_obj = (GameObject)Instantiate(move_light2_GO);
-			moveLight_2.obj_list.Add(extra_obj);
+			GameObject extra_obj = (GameObject)Instantiate(projetil2_GO);
+			projetil2.obj_list.Add(extra_obj);
+			print("projetil 2 expanded");
 			return extra_obj;
 		}
-		
+
+		print("projetil 2 send null");
 		return null;
 	}
 
@@ -100,7 +107,7 @@ public class asset_manager : MonoBehaviour {
 		{
 			if(!fireworks.obj_list[i].activeInHierarchy)
 			{
-				return moveLight_1.obj_list[i];
+				return fireworks.obj_list[i];
 			}	
 		}
 		if (Can_Expand) 
---
+++ launchProjetil.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class launchProjetil : MonoBehaviour {
+
+	public float delay_time;
+	public float repeat_time;
+
+	public enum ProjectilType{projetil1,projetil2,fireWorks};
+
+	public ProjectilType projetil_Type;
+
+	void Start () {
+		InvokeRepeating ("launch", delay_time, repeat_time);
+	}
+
+	void launch () {
+
+		GameObject obj = null;
+
+		switch (projetil_Type) {
+
+			case ProjectilType.projetil1:
+				 obj = asset_manager.current.Get_projetil1 ();
+				break;
+
+			case ProjectilType.projetil2:
+				 obj = asset_manager.current.Get_projetil2 ();
+				break;
+
+			case ProjectilType.fireWorks:
+				obj = asset_manager.current.Get_fireworks (); 
+				break;
+		}
+
+		 if(obj == null)
+			return;
+
+		obj.transform.position = transform.position;
+		obj.transform.rotation = transform.rotation;
+		obj.SetActive (true);
+	}
+}
